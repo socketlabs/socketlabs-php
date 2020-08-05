702413e198611116428b99e448a9582be5d45ede
mirriamronoh <68564150+mirriamronoh@users.noreply.github.com>
2020-08-05 10:41:51 -0400

    Bulk with ampBody support-
    -Created a bulk message file that sends a ampBody supported message

---
+++ InjectionApi/Examples/ExampleCode/Bulk/BulkWithAmpBody.php
@@ -0,0 +1,41 @@
+<?php 
+include_once (__DIR__ . "../../includes.php");
+
+use Socketlabs\Message\BulkMessage;
+use Socketlabs\Message\BulkRecipient;
+use Socketlabs\SocketLabsClient;
+
+$client = new SocketLabsClient(exampleConfig::serverId(), exampleConfig::password()); 
+
+//Build the message
+$message = new BulkMessage();
+
+$message->subject = "Sending A Bulk Message with ampBody";
+$message->htmlBody = "<html>This is the Html Body of my message.</html>";
+$message->plainTextBody = "This is the Plain Text Body of my message.";
+$message->ampBody = "<!doctype html>" +
+"<html amp4email>" +
+"<head>" +
+"  <meta charset=\"utf-8\">" +
+"  <script async src=\"https://cdn.ampproject.org/v0.js\"></script>" +
+"  <style amp4email-boilerplate>body{visibility:hidden}</style>" +
+"  <style amp-custom>" +
+"    h1 {" +
+"      margin: 1rem;" +
+"    }" +
+"  </style>" +
+"</head>" +
+"<body>" +
+"  <h1>This is the AMP Html Body of the message</h1>" +
+"</body>" +
+"</html>";
+$message->from = new BulkRecipient("from@example.com");
+$message->replyTo = new BulkRecipient("replyto@example.com");
+
+//Add Bulk Recipients
+$message->addToAddress (new BulkRecipient("recipient1@example.com", "Recipient #1"));
+$message->addToAddress (new BulkRecipient("recipient2@example.com", "Recipient #2"));
+$message->addToAddress (new BulkRecipient("recipient3@example.com", "Recipient #3"));
+$message->addToAddress (new BulkRecipient("recipient4@example.com", "Recipient #4"));
+ 
+$response = $client->send($message);
