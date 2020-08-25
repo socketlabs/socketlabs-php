8a0d7b84a240100b5fd55c244bbfca2616417505
David Schrenker <david.schrenker@socketlabs.com>
2020-08-25 14:04:59 -0400

    changing environment variable names

--- InjectionApi/Examples/ExampleCode/exampleConfig.php
+++ InjectionApi/Examples/ExampleCode/exampleConfig.php
@@ -1,19 +1,19 @@
 <?php
 class exampleConfig{
- 
+
     public function __construct(){
-         
+
     }
 
     public static function serverId(){
-        return getenv('SocketlabsInjectionServerId');
+        return getenv('SOCKETLABS_SERVER_ID');
     }
 
     public static function password(){
-        return getenv('SocketlabsInjectionApiKey', true) ?: getenv('SocketlabsInjectionApiKey');
+        return getenv('SOCKETLABS_INJECTION_API_KEY', true) ?: getenv('SOCKETLABS_INJECTION_API_KEY');
     }
 
-    public static function proxy() { 
+    public static function proxy() {
         return "http://127.0.0.1:8888";
     }
 
