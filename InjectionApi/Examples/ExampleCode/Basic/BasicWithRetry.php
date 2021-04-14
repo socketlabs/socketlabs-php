a997e855a09f6767e3106a52ddc4d0b6e82e09c3
PraneethChandra <praneeth.chandra@socketlabs.com>
2021-04-14 14:11:09 -0700

    added basic retry example

---
+++ InjectionApi/Examples/ExampleCode/Basic/BasicWithRetry.php
@@ -0,0 +1,25 @@
+<?php
+include_once (__DIR__ . "../../includes.php");
+
+use Socketlabs\Message\BasicMessage;
+use Socketlabs\Message\EmailAddress;
+use Socketlabs\SocketLabsClient;
+
+$client = new SocketLabsClient(exampleConfig::serverId(), exampleConfig::password());   
+$client->proxyUrl = exampleConfig::proxy(); 
+$client->requestTimeout = 5;
+$client->numberOfRetries = 0;
+
+
+ 
+//Build the message
+$message = new BasicMessage(); 
+
+$message->subject = "Sending A Test Message With Retry";
+$message->htmlBody = "<html>This is the Html Body of my message.</html>";
+$message->plainTextBody = "This is the Plain Text Body of my message.";
+$message->from = new EmailAddress("from@example.com");
+$message->addToAddress(new EmailAddress("recipient1@example.com", "Recipient #1"));
+ 
+//Send the message
+$response = $client->send($message);
