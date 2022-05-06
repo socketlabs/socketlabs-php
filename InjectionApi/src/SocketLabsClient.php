19070e71bb3e47737c33ba24bf6b82191e328f93
David Schrenker <david.schrenker@socketlabs.com>
2022-05-06 15:13:17 -0400

    Update SocketLabsClient.php
    Up version

--- InjectionApi/src/SocketLabsClient.php
+++ InjectionApi/src/SocketLabsClient.php
@@ -19,7 +19,7 @@ class SocketLabsClient{
         private $serverId;
         private $apiKey;
 
-        const VERSION = "1.2.1";
+        const VERSION = "1.2.2";
         public $version = self::VERSION;
 
         /**
